65ef87499337a2c68197e9ee5ab464ee5a856a59
yvngmaxe <masaya.10609@gmail.com>
2025-10-10 12:17:23 +0900

    sentence

--- components/LPcompany/index.tsx
+++ components/LPcompany/index.tsx
@@ -34,7 +34,7 @@ export default function LPcompany({
           viewport={{ once: true, amount: 0.4 }}
           variants={variants}
         >
-          <p className={styles.kicker}>ABOUT US -CANVASについて-</p>
+          <p className={styles.kicker}>ABOUT US -わたしたちについて-</p>
           <h2 id="lpcompany-title" className={styles.title} data-reveal>
             {title}
           </h2>
